8f5448f0087bf7f661d304e69c2463b0e6fe6968
ElviBoka <elvi.boka.donbosko@gmail.com>
2025-09-24 11:20:29 +0200

    Add files via upload

---
+++ smoke_suite.py
@@ -0,0 +1,8 @@
+import os
+print("RUN io_echo.py (shtyp nje fjale kur kerkohet):")
+os.system("python io_echo.py")
+print("RUN file_roundtrip.py:")
+os.system("python file_roundtrip.py")
+print("RUN path_probe.py:")
+os.system("python path_probe.py")
+print("ALL GOOD")
